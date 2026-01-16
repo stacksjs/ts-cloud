8169bcdb9276f5a27302c81d28a7b69376edb07e
glennmichael123 <gtorregosa@gmail.com>
2026-01-16 23:56:44 +0800

    chore: wip

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * ts-cloud - Infrastructure as Code for TypeScript
+ *
+ * This is the root entry point that re-exports from the main ts-cloud package.
+ */
+export * from '@ts-cloud/core'
+export * from '@ts-cloud/types'
+export * from '@ts-cloud/aws-types'
