a34c6d7e558be5bab0e59d944c2f33da7154db35
glennmichael123 <gtorregosa@gmail.com>
2026-04-14 17:39:17 +0800

    chore: fix lint errors

--- scripts/local-stop.sh
+++ scripts/local-stop.sh
@@ -6,10 +6,10 @@
 set -e
 
 # Colors for output
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-NC='\033[0m' # No Color
+RED='\033[ 0;31m'
+GREEN='\033[ 0;32m'
+YELLOW='\033[ 1;33m'
+NC='\033[ 0m' # No Color
 
 echo -e "${GREEN}Stopping ts-cloud local development environment...${NC}"
 
